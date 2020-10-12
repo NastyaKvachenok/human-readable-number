5d6465663333b3c8c38e0fe299850080b2347528
NastyaKvachenok <nastya.kvachenok@gmail.com>
2020-10-12 20:49:39 +0300

    feat: implement human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
+
 module.exports = function toReadable (number) {
-  
+  let array1 = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+  let array2 = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+  let humanReadableNumber = '';
+  if (number === 0) {
+    return "zero";
+  }
+  function getNumberToWord(number) {
+    
+    if (number < 20) {
+      humanReadableNumber = humanReadableNumber + array1[number];
+      return humanReadableNumber;
+    }
+
+    if (number < 100) {
+      let x = Math.floor(number/10);
+      humanReadableNumber = humanReadableNumber + array2[x] + " ";
+      return getNumberToWord(number - x * 10);
+    }
+
+    if (number < 1000) {
+      x = Math.floor(number/100);
+      humanReadableNumber = humanReadableNumber + array1[x] + " hundred" + " ";
+      return getNumberToWord(number - x * 100);
+    }
+  }
+
+  return getNumberToWord(number).trim();
 }
